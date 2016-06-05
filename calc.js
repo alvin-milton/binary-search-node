823a06de30641e9452a9fde4c4976b3fcc2a5e4f
Alvin Milton <vinnypesos@gmail.com>
2016-06-05 12:25:22 -0400

    better output

--- calc.js
+++ calc.js
@@ -4,32 +4,34 @@ var grid = new Grid();
 var Calc = function() {};
 var oldMin = 0;
 
+Calc.prototype.lowGuess = function (guess, oldMin) {
+    console.log('\nIs the number: ' + guess + '?');
+    console.log('\nMy chosen number is less than ' + guess + ' but more than ' + oldMin);
+};
+
+Calc.prototype.highGuess = function (guess) {
+    console.log('\nIs the number: ' + guess + '?');
+    console.log('\nMy chosen number is more than ' + guess);
+};
+
 Calc.prototype.find = function (min, max, chosen) {
     var guess;
     if (chosen < parseInt((min + max) / 2, 10)) {
         guess = parseInt((min + max) / 2, 10);
         max = guess - 1;
-        console.log();
-        console.log('Is the number: ' + guess + '?');
-        console.log();
-        console.log('My chosen number is less than ' + guess + ' but more than ' + oldMin);
+        this.lowGuess(guess, oldMin);
         newGrid = grid.return(oldMin, max);
-        // console.log(newGrid);
         this.find(oldMin, max, chosen);
     } else if (chosen > parseInt((min + max) / 2, 10)) {
         guess = parseInt((min + max) / 2, 10);
-        console.log();
-        console.log('Is the number: ' + guess + '?');
-        console.log();
-        console.log('My chosen number is more than ' + guess);
+        this.highGuess(guess);
         min = guess + 1;
         newGrid = grid.return(guess + 1, max);
-        // console.log(newGrid);
         oldMin = min;
         this.find(min, max, chosen);
     } else {
         console.log('The chosen number is ' + chosen);
-        return false;
+        // return false;
     }
 }
 
