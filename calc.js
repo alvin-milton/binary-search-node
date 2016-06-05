12e636c62592964a6b1621d4186cbfa6e4c2be68
Alvin Milton <vinnypesos@gmail.com>
2016-06-05 12:18:44 -0400

    No arrs returned, better output

--- calc.js
+++ calc.js
@@ -9,20 +9,26 @@ Calc.prototype.find = function (min, max, chosen) {
     if (chosen < parseInt((min + max) / 2, 10)) {
         guess = parseInt((min + max) / 2, 10);
         max = guess - 1;
-        console.log(chosen + ' is less than ' + guess + ' but more than ' + oldMin);
+        console.log();
+        console.log('Is the number: ' + guess + '?');
+        console.log();
+        console.log('My chosen number is less than ' + guess + ' but more than ' + oldMin);
         newGrid = grid.return(oldMin, max);
-        console.log(newGrid);
+        // console.log(newGrid);
         this.find(oldMin, max, chosen);
     } else if (chosen > parseInt((min + max) / 2, 10)) {
         guess = parseInt((min + max) / 2, 10);
-        console.log(chosen + ' is more than ' + guess + ' but less than ' + min);
+        console.log();
+        console.log('Is the number: ' + guess + '?');
+        console.log();
+        console.log('My chosen number is more than ' + guess);
         min = guess + 1;
         newGrid = grid.return(guess + 1, max);
-        console.log(newGrid);
+        // console.log(newGrid);
         oldMin = min;
         this.find(min, max, chosen);
     } else {
-        console.log(chosen + ' has been found!');
+        console.log('The chosen number is ' + chosen);
         return false;
     }
 }
